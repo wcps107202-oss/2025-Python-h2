cc9169c888f1e06e6158444d812bde7addda6a0b
chiyanglin <chiyanglin@gmail.com>
2025-04-29 20:56:14 +0800

    Create ex7-1.py

---
+++ ex7-1.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    return x + y
+def mul(x, y):
+    return x * y
+x = int(input("please input num x:"))
+y = int(input("please input num y:"))
+print("x+y =",add(x, y))
+print("x*y =",mul(x, y))
