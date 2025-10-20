37239000928a98d05ee286bdf1ea1c9b6d2c3154
CodingCoffee <88169585+CodingCoffee-01@users.noreply.github.com>
2025-10-20 16:52:58 +0800

    Delete ex7-2.py

--- ex7-2.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 1:
-        #print(n)
-        return 1
-    #else:
-       #print(n)
-    return  n * factorial(n - 1)
-
-print(factorial(5)) # factorial(5) = 5 *4 *3 *2 *1
